d8220f94bf65437b9c9ce868a5926417dff0e23e
Oskar Hahn <mail@oshahn.de>
2017-01-12 00:08:32 +0100

    Added first flags to define the host and port to listen on

--- main.go
+++ main.go
@@ -4,13 +4,16 @@ This is an asgi protocol server and should be an alternative to daphne.
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/gorilla/websocket"
+	"github.com/urfave/cli"
 
 	"github.com/ostcar/goasgiserver/asgi"
 	"github.com/ostcar/goasgiserver/asgi/redis"
-
-	"github.com/gorilla/websocket"
 )
 
 var channelLayer asgi.ChannelLayer
@@ -45,8 +48,28 @@ func httpLogger(handler http.Handler) http.Handler {
 }
 
 func main() {
-	listen := ":8000"
-	http.HandleFunc("/", asgiHandler)
-	log.Printf("Start webserver to listen on %s", listen)
-	log.Fatal(http.ListenAndServe(listen, httpLogger(http.DefaultServeMux)))
+	app := cli.NewApp()
+	app.Name = "goasgiserver"
+	app.Usage = "an asgi protocol server"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "host, H",
+			Value: "localhost",
+			Usage: "host to listen on",
+		},
+		cli.Int64Flag{
+			Name:  "port, p",
+			Value: 8000,
+			Usage: "port to listen on",
+		},
+	}
+	app.Action = func(c *cli.Context) error {
+		listen := fmt.Sprintf("%s:%d", c.String("host"), c.Int64("port"))
+		http.HandleFunc("/", asgiHandler)
+		log.Printf("Start webserver to listen on %s", listen)
+		log.Fatal(http.ListenAndServe(listen, httpLogger(http.DefaultServeMux)))
+		return nil
+	}
+	app.Run(os.Args)
+
 }
